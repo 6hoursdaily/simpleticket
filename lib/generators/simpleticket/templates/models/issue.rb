c6cd4e67dd90c5c73c1f5dff3207cc94bc37eee8
Rhys Copeland <rhys@iff-transponder.co.uk>
2011-06-22 19:53:15 +0100

    Updated index template.

--- lib/generators/simpleticket/templates/models/issue.rb
+++ lib/generators/simpleticket/templates/models/issue.rb
@@ -1,2 +1,17 @@
 class Issue < ActiveRecord::Base
+  include ActionView::Helpers::TextHelper
+  
+  validates_presence_of :description
+  
+  def summary_text
+    if self.summary.nil? or self.summary.empty?
+      return truncate(self.description)
+    else
+      return self.summary
+    end
+  end
+  
+  def status
+    self.fixed ? 'Fixed' : 'Open'
+  end
 end
